4b182be051d6560bd631587ca94d6906aa657c37
Herberton <herberton.dev@gmail.com>
2024-02-01 17:30:48 -0400

    Criação das classes service

---
+++ src/main/java/com/herbertonpjcmt/service/UnidadeService.java
@@ -0,0 +1,11 @@
+package com.herbertonpjcmt.service;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import lombok.AllArgsConstructor;
+
+@ApplicationScoped
+@AllArgsConstructor
+public class UnidadeService {
+
+
+}
