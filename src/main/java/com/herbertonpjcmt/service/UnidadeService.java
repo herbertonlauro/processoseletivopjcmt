806190822b0c7bd4ae73a0db52a4df24ff662d21
Herberton <herberton.dev@gmail.com>
2024-02-09 11:53:03 -0400

    up

--- src/main/java/com/herbertonpjcmt/service/UnidadeService.java
+++ src/main/java/com/herbertonpjcmt/service/UnidadeService.java
@@ -1,12 +1,26 @@
 package com.herbertonpjcmt.service;
 
+import com.herbertonpjcmt.dto.UnidadeDTO;
+import com.herbertonpjcmt.mappers.UnidadeMapper;
+import com.herbertonpjcmt.model.Unidade;
+import com.herbertonpjcmt.repository.UnidadeRepository;
 import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
 import lombok.AllArgsConstructor;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @ApplicationScoped
 public class UnidadeService {
 
+    @Inject
+    UnidadeRepository unidadeRepository;
+
+    @Inject
+    UnidadeMapper unidadeMapper;
+
+    public List<UnidadeDTO> listDTO = new ArrayList<>();
+    List<Unidade> listaUnidade = unidadeRepository.listAll();
 
 }
