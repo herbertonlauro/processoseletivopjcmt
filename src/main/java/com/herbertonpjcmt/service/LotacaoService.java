736cfcd831c82f9a3eed164a10360ee8347b732c
Herberton <herberton.dev@gmail.com>
2024-02-09 15:54:28 -0400

    service lotação

--- src/main/java/com/herbertonpjcmt/service/LotacaoService.java
+++ src/main/java/com/herbertonpjcmt/service/LotacaoService.java
@@ -1,6 +1,13 @@
 package com.herbertonpjcmt.service;
 
+import com.herbertonpjcmt.dto.LotacaoDTO;
+
+import java.util.List;
+
 public class LotacaoService {
 
+    public List<LotacaoDTO> listar(){
 
+        return null;
+    }
 }
