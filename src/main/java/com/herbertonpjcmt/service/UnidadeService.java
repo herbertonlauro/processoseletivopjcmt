8e38ad3eea7da2cff2464691dd26aaecad87c50d
herbertonlauro <herberton.dev@gmail.com>
2024-02-08 22:45:38 -0400

    Criado metodo listar e inserir

--- src/main/java/com/herbertonpjcmt/service/UnidadeService.java
+++ src/main/java/com/herbertonpjcmt/service/UnidadeService.java
@@ -1,12 +1,36 @@
 package com.herbertonpjcmt.service;
 
+import com.herbertonpjcmt.dto.UnidadeDTO;
+import com.herbertonpjcmt.mappers.UnidadeMapper;
+import com.herbertonpjcmt.model.Unidade;
+import com.herbertonpjcmt.repository.UnidadeRepository;
 import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
 import lombok.AllArgsConstructor;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @ApplicationScoped
 public class UnidadeService {
+
+    @Inject
+    UnidadeMapper unidadeMapper;
+
+    @Inject
+    UnidadeRepository unidadeRepository;
+
+    public List<UnidadeDTO> listar(){
+        List<Unidade> unidades = new ArrayList<>();
+        return unidades.stream().map(unidadeMapper::unidadeToDTO).collect(Collectors.toList());
+    }
+
+    public UnidadeDTO inserir(UnidadeDTO unidadeDTO){
+        Unidade unidade = unidadeMapper.DTOtoUnidade(unidadeDTO);
+        unidadeRepository.persist(unidade);
+        return unidadeMapper.unidadeToDTO(unidade);
+    }
 
 
 }
