b6434711351686e652d0b597a6dd6b4471141f69
Herberton <herberton.dev@gmail.com>
2024-02-20 13:59:08 -0400

    Criado post unidade controller

--- src/main/java/com/herbertonpjcmt/controllers/UnidadeController.java
+++ src/main/java/com/herbertonpjcmt/controllers/UnidadeController.java
@@ -3,13 +3,14 @@ package com.herbertonpjcmt.controllers;
 import com.herbertonpjcmt.dto.UnidadeDTO;
 import com.herbertonpjcmt.service.UnidadeService;
 import jakarta.inject.Inject;
-import jakarta.ws.rs.Consumes;
-import jakarta.ws.rs.GET;
-import jakarta.ws.rs.Path;
-import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+import org.eclipse.microprofile.openapi.annotations.Operation;
+import org.eclipse.microprofile.openapi.annotations.parameters.RequestBody;
 import org.eclipse.microprofile.openapi.annotations.tags.Tag;
 
+import java.net.URI;
 import java.util.List;
 
 @Produces(MediaType.APPLICATION_JSON)
@@ -27,4 +28,11 @@ public class UnidadeController {
         return unidadeService.listar();
     }
 
+    @Operation(summary = "Cadastro de unidade", description = "Cadastrar unidade")
+    @POST
+    public Response criar(@RequestBody UnidadeDTO unidadeDTO){
+        UnidadeDTO unidade = unidadeService.inserir(unidadeDTO);
+        return Response.created(URI.create("/unidades")).entity(unidade).build();
+    }
 }
+
